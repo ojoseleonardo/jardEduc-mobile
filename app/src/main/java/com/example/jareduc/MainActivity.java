8e47c6a899b7667bbdb138b4955f871b627e01a2
José Leonardo <joseleonardosantoos@outlook.com>
2022-10-24 04:16:17 -0300

    add menu e fragments

---
+++ app/src/main/java/com/example/jareduc/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jareduc;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.jareduc.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        getSupportActionBar().hide();
+        setContentView(binding.getRoot());
+
+        initNavigation();
+    }
+
+    private void initNavigation(){
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+}
